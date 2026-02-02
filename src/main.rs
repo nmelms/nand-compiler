3314443dc581fc2bc5ee019eb8333fb36bddf19b
Nick Melms <nmelms92@gmail.com>
2026-02-01 16:52:26 -0800

    detect symbol and skip newlines

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,21 @@ fn main() {
     println!("running in main");
 
     let mut tokenizer = Tokenizer::new("test.jack");
+    println!("{}", tokenizer.current_token);
     tokenizer.advance();
-    tokenizer.advance();
-    tokenizer.advance();
+    println!("{}", tokenizer.current_token);
 
+    tokenizer.advance();
+    println!("{}", tokenizer.current_token);
+    tokenizer.advance();
+    println!("{}", tokenizer.current_token);
 
+        tokenizer.advance();
+    println!("{}", tokenizer.current_token);
+
+            tokenizer.advance();
+    println!("{}", tokenizer.current_token);
+
+            tokenizer.advance();
+    println!("{}", tokenizer.current_token);
 }
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -3,12 +3,18 @@ use std::fs;
 pub struct Tokenizer {
     chars: Vec<char>,
     i: usize,
-    current_token: Option<Token>,
+    pub current_token: String,
 }
 
-pub enum Token {
-
-}
+// #[derive(Debug, Clone, Copy, PartialEq, Eq)]
+// pub enum TokenType {
+//     Keyword,
+//     Symbol,
+//     Identifier,
+//     IntConst,
+//     StringConst,
+//     None,
+// }
 
 impl Tokenizer {
     pub fn new(path: &str) -> Self {
@@ -17,14 +23,14 @@ impl Tokenizer {
             .chars()
             .collect();
         let i = 0;
-        let current_token = None;
+        let current_token = String::from("None");
 
-        println!("{:?}", chars);
-        Self { chars, i, current_token }
-
-        
+        Self {
+            chars,
+            i,
+            current_token,
+        }
     }
-
 
     pub fn has_more_tokens(&self) -> bool {
         if self.i < self.chars.len() {
@@ -36,23 +42,29 @@ impl Tokenizer {
 
     pub fn advance(&mut self) {
         let mut s: String = String::from("");
-        let mut cur = self.chars[self.i];
 
         // skip spaces
-        while cur == ' ' {
+        while self.chars[self.i] == ' ' || self.chars[self.i] == '\n' {
             self.i += 1;
-            cur =  self.chars[self.i];
         }
 
         // consume keyword
-        if cur.is_ascii_alphabetic(){
+        if self.chars[self.i].is_ascii_alphabetic() {
             while self.chars[self.i].is_ascii_alphabetic() {
                 s.push(self.chars[self.i]);
                 self.i += 1;
             }
+            self.current_token = s;
         }
 
+        //consume symbol
+        if Tokenizer::is_symbol(self.chars[self.i]) {
+            self.current_token = self.chars[self.i].to_string();
+            self.i += 1;
+        }
+    }
 
-        println!("exit while. s: {}", s)
+    fn is_symbol(cur: char) -> bool {
+        "{}()[].,;+-*/&|<>=~".contains(cur)
     }
 }
