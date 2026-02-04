f98cfce7b5ee8ddca99b1e314dd0b4e64c2c6277
Nick Melms <nmelms92@gmail.com>
2026-02-03 17:15:12 -0800

    output xml

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,45 @@
 use crate::tokenizer::{TokenType, Tokenizer};
+use std::fs;
+use std::io::Write;
 
 mod tokenizer;
 
 fn main() {
     println!("running in main");
     let mut tokenizer = Tokenizer::new("test.jack");
+    let mut output = fs::File::create("outputT.xml").expect("failed to create file");
 
     while tokenizer.has_more_tokens() {
         println!("{}", tokenizer.current_token);
-        if let Some(TokenType::Keyword) = tokenizer.current_token_type.as_ref() {
-            println!("it was a keyword: {}", tokenizer.current_token);
+
+        match tokenizer.current_token_type {
+            Some(TokenType::Identifier) => writeln!(
+                output,
+                "<identifier>{}</identifier>",
+                tokenizer.current_token
+            )
+            .expect("failed to write"),
+            Some(TokenType::Keyword) => {
+                writeln!(output, "<keyword>{}</keyword>", tokenizer.current_token)
+                    .expect("failed to write")
+            }
+            Some(TokenType::Symbol) => {
+                writeln!(output, "<symbol>{}</symbol>", tokenizer.current_token)
+                    .expect("failed to write")
+            }
+            Some(TokenType::IntConst) => writeln!(
+                output,
+                "<intergerConstant>{}</intergetConstant>",
+                tokenizer.current_token
+            )
+            .expect("failed to write"),
+            Some(TokenType::StringConst) => writeln!(
+                output,
+                "<stringConstant>{}</stringConstant>",
+                tokenizer.current_token
+            )
+            .expect("failed to write"),
+            _ => println!("no match"),
         }
 
         tokenizer.advance();
