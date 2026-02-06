b54c351dc6a08eeb4d20d8ed8d8628157571275f
Nick Melms <nmelms92@gmail.com>
2026-02-06 15:19:16 -0800

    fix out of bounds panic bug and create compile subroutine

--- src/compilation_engine.rs
+++ src/compilation_engine.rs
@@ -23,16 +23,122 @@ impl ComplationEngine {
         while self.tokenizer.current_token == "static" || self.tokenizer.current_token == "field" {
             self.compile_class_var_dec();
         }
+        while self.tokenizer.current_token == "constructor"
+            || self.tokenizer.current_token == "function"
+            || self.tokenizer.current_token == "method"
+        {
+            self.compile_subroutine();
+        }
+
         self.process("}".to_string());
+
         writeln!(self.output, "</class>").unwrap();
     }
 
-    fn compile_class_var_dec(&mut self){
+    fn compile_subroutine(&mut self) {
+        //
+        match self.tokenizer.current_token.as_str() {
+            "constructor" => self.process("constructor".to_string()),
+            "function" => self.process("function".to_string()),
+            "method" => self.process("method".to_string()),
+            _ => {
+                eprintln!(
+                    "Syntax error: expected (contructor | function | method), got '{}'",
+                    self.tokenizer.current_token
+                );
+                std::process::exit(1);
+            }
+        }
+        // params
+        // ('void|type)
+        match self.tokenizer.current_token_type {
+            Some(TokenType::Keyword) => match self.tokenizer.current_token.as_str() {
+                "void" => self.process(self.tokenizer.current_token.to_string()),
+                "int" => self.process("int".to_string()),
+                "char" => self.process("char".to_string()),
+                "boolean" => self.process("boolean".to_string()),
+                _ => print!("something fucked up"),
+            },
+            Some(TokenType::Identifier) => self.process(self.tokenizer.current_token.to_string()),
+            None => {
+                eprintln!("Syntax error: got '{}'", self.tokenizer.current_token);
+                std::process::exit(1);
+            }
+            _ => {
+                eprintln!(
+                    "Syntax error: expected (void | type), got '{}'",
+                    self.tokenizer.current_token
+                );
+                std::process::exit(1);
+            }
+        }
+        // subroutine name
+        if self.tokenizer.current_token_type == Some(TokenType::Identifier) {
+            self.process(self.tokenizer.current_token.to_string());
+        } else {
+            println!(
+                "Syntax error: expected subroutine name '{}'",
+                self.tokenizer.current_token
+            );
+            std::process::exit(1);
+        }
+
+        self.process("(".to_string());
+
+        if self.tokenizer.current_token != ")" {
+            // Process type
+            match self.tokenizer.current_token_type {
+                Some(TokenType::Identifier) => {
+                    self.process(self.tokenizer.current_token.to_string())
+                }
+                _ => match self.tokenizer.current_token.as_str() {
+                    "int" => self.process("int".to_string()),
+                    "char" => self.process("char".to_string()),
+                    "boolean" => self.process("boolean".to_string()),
+                    _ => {
+                        println!(
+                            "Syntax error: expected subroutine name '{}'",
+                            self.tokenizer.current_token
+                        );
+                        std::process::exit(1);
+                    }
+                },
+            }
+            //process varname
+            self.process(self.tokenizer.current_token.to_string());
+            while self.tokenizer.current_token == "," {
+                self.process(",".to_string());
+                // Process type
+                match self.tokenizer.current_token_type {
+                    Some(TokenType::Identifier) => {
+                        self.process(self.tokenizer.current_token.to_string())
+                    }
+                    _ => match self.tokenizer.current_token.as_str() {
+                        "int" => self.process("int".to_string()),
+                        "char" => self.process("char".to_string()),
+                        "boolean" => self.process("boolean".to_string()),
+                        _ => {
+                            println!(
+                                "Syntax error: expected subroutine name '{}'",
+                                self.tokenizer.current_token
+                            );
+                            std::process::exit(1);
+                        }
+                    },
+                }
+                //process varname
+                self.process(self.tokenizer.current_token.to_string());
+            }
+        }
+        self.process(")".to_string());
+    }
+
+    fn compile_class_var_dec(&mut self) {
         writeln!(self.output, "<classVarDec>").unwrap();
         // static and field
-        if self.tokenizer.current_token == "static"{
+        if self.tokenizer.current_token == "static" {
             self.process("static".to_string());
-        }else if self.tokenizer.current_token == "field"{
+        } else if self.tokenizer.current_token == "field" {
             self.process("field".to_string());
         }
         // type
@@ -41,16 +147,18 @@ impl ComplationEngine {
             "char" => self.process("char".to_string()),
             "boolean" => self.process("boolean".to_string()),
             _ => {
-                eprintln!("Syntax error: expected type (int | char | boolean), got '{}'", self.tokenizer.current_token);
+                eprintln!(
+                    "Syntax error: expected type (int | char | boolean), got '{}'",
+                    self.tokenizer.current_token
+                );
                 std::process::exit(1);
             }
         }
         self.process(self.tokenizer.current_token.clone());
         // identify multiple variables
-        while self.tokenizer.current_token == ","{
+        while self.tokenizer.current_token == "," {
             self.process(",".to_string());
             self.process(self.tokenizer.current_token.clone());
-
         }
 
         self.process(";".to_string());
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -67,82 +67,85 @@ impl Tokenizer {
 
     pub fn advance(&mut self) {
         let mut s: String = String::from("");
-
-        // skip spaces
-        while self.chars[self.i] == ' ' || self.chars[self.i] == '\n' {
-            self.i += 1;
-        }
-
-        // consume keyword or identifier
-        if self.chars[self.i].is_ascii_alphabetic() {
-            while self.chars[self.i].is_ascii_alphabetic() {
-                s.push(self.chars[self.i]);
+        //added this if check to prevent panic
+        // not really sure if this is the best approach but it worked. 
+        if self.i < self.chars.len() {
+            // skip spaces
+            while self.chars[self.i] == ' ' || self.chars[self.i] == '\n' {
                 self.i += 1;
             }
-            if KEYWORDS.contains(&s.as_str()) {
-                self.current_token_type = Some(TokenType::Keyword);
-            } else {
-                self.current_token_type = Some(TokenType::Identifier);
-            }
-            self.current_token = s;
-            return;
-        }
 
-        //consume symbol
-        if Tokenizer::is_symbol(self.chars[self.i]) {
-            let special_symbols = &['<', '>', '"', '&'];
-
-            if special_symbols.contains(&self.chars[self.i]) {
-                match &self.chars[self.i] {
-                    '<' => self.current_token = "&lt;".to_string(),
-                    '>' => self.current_token = "&gt;".to_string(),
-                    '"' => self.current_token = "&quot;".to_string(),
-                    '&' => self.current_token = "amp;".to_string(),
-                    _ => println!("something weird. happened"),
+            // consume keyword or identifier
+            if self.chars[self.i].is_ascii_alphabetic() {
+                while self.chars[self.i].is_ascii_alphabetic() {
+                    s.push(self.chars[self.i]);
+                    self.i += 1;
                 }
-            } else {
-                self.current_token = self.chars[self.i].to_string();
+                if KEYWORDS.contains(&s.as_str()) {
+                    self.current_token_type = Some(TokenType::Keyword);
+                } else {
+                    self.current_token_type = Some(TokenType::Identifier);
+                }
+                self.current_token = s;
+                return;
             }
-            self.current_token_type = Some(TokenType::Symbol);
 
-            self.i += 1;
-            return;
-        }
+            //consume symbol
+            if Tokenizer::is_symbol(self.chars[self.i]) {
+                let special_symbols = &['<', '>', '"', '&'];
 
-        //consume int
-        if self.chars[self.i].is_ascii_digit() {
-            while self.chars[self.i].is_ascii_digit() {
-                s.push(self.chars[self.i]);
-                self.i += 1;
-            }
-            self.current_token_type = Some(TokenType::IntConst);
-            self.current_token = s;
-            return;
-        }
-
-        // consume string
-        if self.chars[self.i] == '"' {
-            let special_symbols = &['<', '>', '"', '&'];
-            self.i += 1;
-            while self.chars[self.i] != '"' {
                 if special_symbols.contains(&self.chars[self.i]) {
                     match &self.chars[self.i] {
-                        '<' => s.push_str("&lt;"),
-                        '>' => s.push_str("&gt;"),
-                        '"' => s.push_str("&quot;"),
-                        '&' => s.push_str("&amp;"),
+                        '<' => self.current_token = "&lt;".to_string(),
+                        '>' => self.current_token = "&gt;".to_string(),
+                        '"' => self.current_token = "&quot;".to_string(),
+                        '&' => self.current_token = "amp;".to_string(),
                         _ => println!("something weird. happened"),
                     }
                 } else {
-                    s.push(self.chars[self.i]);
+                    self.current_token = self.chars[self.i].to_string();
                 }
+                self.current_token_type = Some(TokenType::Symbol);
 
                 self.i += 1;
+                return;
             }
-            self.i += 1;
-            self.current_token_type = Some(TokenType::StringConst);
-            self.current_token = s;
-            return;
+
+            //consume int
+            if self.chars[self.i].is_ascii_digit() {
+                while self.chars[self.i].is_ascii_digit() {
+                    s.push(self.chars[self.i]);
+                    self.i += 1;
+                }
+                self.current_token_type = Some(TokenType::IntConst);
+                self.current_token = s;
+                return;
+            }
+
+            // consume string
+            if self.chars[self.i] == '"' {
+                let special_symbols = &['<', '>', '"', '&'];
+                self.i += 1;
+                while self.chars[self.i] != '"' {
+                    if special_symbols.contains(&self.chars[self.i]) {
+                        match &self.chars[self.i] {
+                            '<' => s.push_str("&lt;"),
+                            '>' => s.push_str("&gt;"),
+                            '"' => s.push_str("&quot;"),
+                            '&' => s.push_str("&amp;"),
+                            _ => println!("something weird. happened"),
+                        }
+                    } else {
+                        s.push(self.chars[self.i]);
+                    }
+
+                    self.i += 1;
+                }
+                self.i += 1;
+                self.current_token_type = Some(TokenType::StringConst);
+                self.current_token = s;
+                return;
+            }
         }
     }
     pub fn keyword(&self) -> Option<&TokenType> {
