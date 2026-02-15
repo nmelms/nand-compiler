cfdb2cea5aed957ba9029e9b2ab3c7f3a44ceb81
Nick Melms <nmelms92@gmail.com>
2026-02-14 16:12:13 -0800

    implement define

--- src/symbol_table.rs
+++ src/symbol_table.rs
@@ -10,14 +10,21 @@ pub struct SymbolTable {
 }
 
 pub struct Symbol{
-    name: String,
-    r#type: String, 
-    kind: String,
+    symbol_type: String, 
+    kind: SymbolType,
+    index: usize,
+}
+
+pub enum SymbolType{
+    Static,
+    Field, 
+    Arg,
+    Var,
 }
 
 
 impl SymbolTable{
-  pub fn new() -> Self{
+    pub fn new() -> Self{
     return Self{ 
         table: HashMap::new(), 
         static_index: 0, 
@@ -25,15 +32,46 @@ impl SymbolTable{
         arg_index: 0,
         var_index: 0 
     };
-  }
+    }
 
-  pub fn reset(&mut self){
+    pub fn reset(&mut self){
         self.table = HashMap::new(); 
         self.static_index = 0;
         self.field_index =  0;
         self.arg_index = 0;
         self.var_index = 0;
-  }
+    }
+
+    pub fn define(&mut self, name:String, symbol_type: String, kind: SymbolType){
+        let index = match kind{
+            SymbolType::Static => {
+                let temp = self.static_index;
+                self.static_index += 1;
+                temp
+            },
+            SymbolType::Field => {
+                let temp = self.field_index;
+                self.field_index += 1;
+                temp
+            },
+            SymbolType::Arg => {
+                let temp = self.arg_index;
+                self.arg_index += 1;
+                temp
+            },
+            SymbolType::Var => {
+                let temp = self.var_index;
+                self.var_index += 1;
+                temp
+            },
+        };
+
+        self.table.insert(name,  Symbol { 
+            symbol_type: symbol_type, 
+            kind: kind, 
+            index: index
+        });
+    }
 
 
-}
+} 
